d4cf01650131848d98f5c08d632d1aba412e0003
reyval <yudhareyval4@gmail.com>
2024-02-07 14:08:23 +0700

    shorted the delay

--- lowes-by-sku.js
+++ lowes-by-sku.js
@@ -181,7 +181,7 @@ const setLowesStore = async page => {
 			await delay(60000)
 			console.log('Start VPN')
 			await vpn.start()
-			await delay(600000)
+			await delay(60000)
 			let ip = await checkIp(1)
 			await delay(5000)
 		}
